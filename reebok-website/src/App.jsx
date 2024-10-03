d807e3983a0b50a050ff66ec569a788869f6b152
Siratoon Nahar <97539283+SiratoonNahar@users.noreply.github.com>
2024-10-03 15:04:45 +0530

    -feature cards has been created

--- reebok-website/src/App.jsx
+++ reebok-website/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
 import CategoryCards from './components/CategoryCards/CategoryCards';
+import FeatureCards from './components/FeatureCards/FeatureCards';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Banner/>
       <CategoryCards/>
+      <FeatureCards/>
     </div>
   )
 }
